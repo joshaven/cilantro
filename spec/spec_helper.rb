cdda6b67a7d7c0eed8d5536a9a7fc11dd6b170f3
Joshaven Potter <joshaven@joshaven.(none)>
2010-04-20 12:53:18 -0400

    Add :mock_responce method to help in testing http responces.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,18 @@
 ENV['RACK_ENV'] = 'test'
 
+begin
+  require 'rack'
+rescue LoadError
+  require 'rubygems'
+  require 'rack'
+end
+
 require File.expand_path(File.join File.dirname(__FILE__), '..', 'lib', 'cilantro')
 Cilantro.load_environment
 
 dependency 'spec', :gem => 'rspec', :env => :test
 dependency 'rack/test', :gem => 'rack-test', :env => :test
-
+dependency 'sinatra/base'
 
 # spec helper methods
 require 'socket'
@@ -18,3 +25,11 @@ def port_in_use?(port)
     return false
   end
 end  
+
+# Example useage:
+#   response = mock_responce '/test', :get
+#   response.body.should eql("Hello World")
+def mock_responce(path, verb='GET', app=Cilantro.app)
+  request = Rack::MockRequest.env_for(path, 'HTTP_VERSION' => '1.1', 'REQUEST_METHOD' => verb.to_s.upcase)
+  Rack::MockResponse.new *Rack::Chunked.new(app).call(request)
+end
