797b1ecd8350e03295930549a082239d762599ae
Joshaven Potter <yourtech@gmail.com>
2010-05-12 18:50:52 -0400

    Update the order of including things in cilantro... This fixes issues with not having gems sandboxed.

--- lib/cilantro.rb
+++ lib/cilantro.rb
@@ -1,20 +1,25 @@
-# TODO: the following line may mess with the sandboxed gems environment
-require 'rubygems'
-
 unless $LOADED_FEATURES.include?('lib/cilantro.rb') or $LOADED_FEATURES.include?('cilantro.rb')
   # APP_ROOT = File.expand_path(File.join File.dirname(__FILE__), "..") unless defined?(APP_ROOT)
   APP_ROOT = Dir.pwd unless defined?(APP_ROOT)
   CILANTRO_ROOT = File.expand_path(File.join File.dirname(__FILE__), "..") unless defined?(CILANTRO_ROOT)
 
   IRB.conf[:PROMPT_MODE] = :SIMPLE if ::Object.const_defined?(:IRB)
-
-  require 'rack'
-  require 'extlib'
-
-  require File.expand_path(File.join CILANTRO_ROOT, 'lib', 'cilantro', 'base')
-  require File.expand_path(File.join CILANTRO_ROOT, 'lib', 'cilantro', 'controller')
-  require File.expand_path(File.join CILANTRO_ROOT, 'lib', 'cilantro', 'templater')
+  
+  # load the core of Cilantro
+  require File.join CILANTRO_ROOT, 'lib', 'cilantro', 'base'
+  
+  # sandbox gems & setup dependency command...
+  require File.join CILANTRO_ROOT, 'lib', 'cilantro', 'dependencies'
+  
+  # Define controller and templater
+  require File.join CILANTRO_ROOT, 'lib', 'cilantro', 'controller'
+  require File.join CILANTRO_ROOT, 'lib', 'cilantro', 'templater'
+  
+  # Permit auto reloading of source files
+  dependency 'rack'
   require File.expand_path(File.join CILANTRO_ROOT, 'lib', 'cilantro', 'auto_reload')
-  #
+  
+  # Dynamic application creation...
+  dependency 'extlib'
   require File.expand_path(File.join CILANTRO_ROOT, 'lib', 'cilantro', 'generator')
 end
