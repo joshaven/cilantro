ccd768315fed91df53f4b3a7f4bdb3b647a3fdb1
Joshaven Potter <yourtech@gmail.com>
2010-07-06 18:31:55 -0400

    Only remove Templates if it is included in the Applicaion

--- lib/cilantro/sinatra.rb
+++ lib/cilantro/sinatra.rb
@@ -5,9 +5,11 @@ dependency 'thin'
 dependency 'eventmachine'
 dependency 'daemons'
 dependency 'sinatra/base', :gem => 'sinatra'
-Sinatra.send(:remove_const, :Templates)
+
+# Sinatra.send(:remove_const, :Templates) if Sinatra.constants.include?('Templates')
 class Application < Sinatra::Base
   include Cilantro::Application
   include Cilantro::Templater if defined? Cilantro::Templater
+  include Cilantro::Helpers if defined? Cilantro::Templater
   enable :sessions
 end
